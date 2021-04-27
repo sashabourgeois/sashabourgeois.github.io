b4e30c7cb47847ea8766a0d23cf96ae98e12c224
sashabourgeois <sasha_bourgeois96@student.stmaryk12.net>
2021-04-27 15:39:05 +0000

    adding circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,12 +20,23 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
+        var circle;
+        var circles = [];        
 
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
 
         // TODO 3 / 7 : Call the drawCircle() function 
+        for (var drawCircle = 0; drawCircle <= 100; drawCircle++){
+
+        
+        }
 
 
         ////////////////////////////////////////////////////////////
@@ -39,12 +50,19 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
+            
 
             
             // TODO 5 : Call game.checkCirclePosition() on your circles.
+            
            
 
             // TODO 8 : Iterate over the array
+            for (var i = 0; i < circles.length; i++){
+                var eachCircle = circles[i];
+                physikz.updatePosition(eachCircle);
+                game.checkCirclePosition(eachCircle);
+            }
            
             
         }
@@ -62,7 +80,10 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if ( circle.y > circle.height) {
+                circle.y = 0;
+            }
+            var rightEdge = circle.x + circle.radius;
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
